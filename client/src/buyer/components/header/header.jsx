463195799983167672dc2364a2b3156c0b037f32
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-05-08 12:05:54 +0300

    Commit routes for buyerhome and login pages

---
+++ client/src/buyer/components/header/header.jsx
@@ -0,0 +1,21 @@
+import '../header/header.css'
+
+function Header(){
+    return(
+        <>
+        <header>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-2">
+                        
+                    </div>
+                </div>
+            </div>
+        </header>
+        </>
+    )
+
+}
+
+export default Header 
+
