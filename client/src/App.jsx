44651e17d7f79e1c57554cedc7e9ce82c1dd3ad0
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-05-08 09:56:03 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Route, Routes, Navigate } from "react-router-dom";
+
+function App() {
+
+
+  return (
+    // Please add your routes and nested routes to this page. 
+    // If there's any problem, reach out and i'll do the routing 
+    <div>
+      <h1>Routing page</h1>
+    </div>
+  )
+}
+
+export default App
